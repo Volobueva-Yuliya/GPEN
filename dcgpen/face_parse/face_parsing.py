0340366418448df7e1ba51a7b653100bcffd5eab
Volobueva-Yuliya <pusifon@gmail.com>
2022-09-22 21:24:19 +0400

    fix import

--- dcgpen/face_parse/face_parsing.py
+++ dcgpen/face_parse/face_parsing.py
@@ -3,15 +3,18 @@
 @author: yangxy (yangtao9009@gmail.com)
 '''
 import os
+
 import cv2
-import torch
 import numpy as np
-from parse_model import ParseNet
+import torch
 import torch.nn.functional as F
+from dcgpen.parse_model import ParseNet
+
 
 class FaceParse(object):
-    def __init__(self, base_dir='./', model='ParseNet-latest', device='cuda'):
-        self.mfile = os.path.join(base_dir, 'weights', model+'.pth')
+    def __init__(self, base_dir='./',
+                 model='ParseNet-latest', device='cuda'):
+        self.mfile = os.path.join(base_dir, 'weights', model + '.pth')
         self.size = 512
         self.device = device
 
@@ -25,13 +28,54 @@ class FaceParse(object):
         18: 'cloth'
         '''
         #self.MASK_COLORMAP = [[0, 0, 0], [204, 0, 0], [76, 153, 0], [204, 204, 0], [51, 51, 255], [204, 0, 204], [0, 255, 255], [255, 204, 204], [102, 51, 0], [255, 0, 0], [102, 204, 0], [255, 255, 0], [0, 0, 153], [0, 0, 204], [255, 51, 153], [0, 204, 204], [0, 51, 0], [255, 153, 51], [0, 204, 0]]
-        #self.#MASK_COLORMAP = [[0, 0, 0], [204, 0, 0], [76, 153, 0], [204, 204, 0], [51, 51, 255], [204, 0, 204], [0, 255, 255], [255, 204, 204], [102, 51, 0], [255, 0, 0], [102, 204, 0], [255, 255, 0], [0, 0, 153], [0, 0, 204], [255, 51, 153], [0, 204, 204], [0, 51, 0], [255, 153, 51], [0, 204, 0]] = [[0, 0, 0], [204, 0, 0], [76, 153, 0], [204, 204, 0], [51, 51, 255], [204, 0, 204], [0, 255, 255], [255, 204, 204], [102, 51, 0], [255, 0, 0], [102, 204, 0], [255, 255, 0], [0, 0, 153], [0, 0, 204], [255, 51, 153], [0, 204, 204], [0, 51, 0], [0, 0, 0], [0, 0, 0]]
-        self.MASK_COLORMAP = [0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, 255, 255, 255, 0]
+        # self.#MASK_COLORMAP = [[0, 0, 0], [204, 0, 0], [76, 153, 0],
+        # [204, 204, 0], [51, 51, 255], [204, 0, 204], [0, 255, 255],
+        # [255, 204, 204], [102, 51, 0], [255, 0, 0], [102, 204, 0],
+        # [255, 255, 0], [0, 0, 153], [0, 0, 204], [255, 51, 153], [0,
+        # 204, 204], [0, 51, 0], [255, 153, 51], [0, 204, 0]] = [[0,
+        # 0, 0], [204, 0, 0], [76, 153, 0], [204, 204, 0], [51, 51,
+        # 255], [204, 0, 204], [0, 255, 255], [255, 204, 204], [102,
+        # 51, 0], [255, 0, 0], [102, 204, 0], [255, 255, 0], [0, 0,
+        # 153], [0, 0, 204], [255, 51, 153], [0, 204, 204], [0, 51,
+        # 0], [0, 0, 0], [0, 0, 0]]
+        self.MASK_COLORMAP = [
+            0,
+            255,
+            255,
+            255,
+            255,
+            255,
+            255,
+            255,
+            255,
+            255,
+            255,
+            255,
+            255,
+            255,
+            0,
+            255,
+            255,
+            255,
+            0]
         self.load_model()
 
     def load_model(self):
-        self.faceparse = ParseNet(self.size, self.size, 32, 64, 19, norm_type='bn', relu_type='LeakyReLU', ch_range=[32, 256])
-        self.faceparse.load_state_dict(torch.load(self.mfile, map_location=torch.device('cpu')))
+        self.faceparse = ParseNet(
+            self.size,
+            self.size,
+            32,
+            64,
+            19,
+            norm_type='bn',
+            relu_type='LeakyReLU',
+            ch_range=[
+                32,
+                256])
+        self.faceparse.load_state_dict(
+            torch.load(
+                self.mfile,
+                map_location=torch.device('cpu')))
         self.faceparse.to(self.device)
         self.faceparse.eval()
 
@@ -44,13 +88,15 @@ class FaceParse(object):
         return mask
 
     def process_tensor(self, imt):
-        imt = F.interpolate(imt.flip(1)*2-1, (self.size, self.size))
+        imt = F.interpolate(
+            imt.flip(1) * 2 - 1, (self.size, self.size))
         pred_mask, sr_img_tensor = self.faceparse(imt)
 
         mask = pred_mask.argmax(dim=1)
         for idx, color in enumerate(self.MASK_COLORMAP):
-            mask = torch.where(mask==idx, color, mask)
-        #mask = mask.repeat(3, 1, 1).unsqueeze(0) #.cpu().float().numpy()
+            mask = torch.where(mask == idx, color, mask)
+        # mask = mask.repeat(3, 1, 1).unsqueeze(0)
+        # #.cpu().float().numpy()
         mask = mask.unsqueeze(0)
 
         return mask
@@ -58,14 +104,17 @@ class FaceParse(object):
     def img2tensor(self, img):
         img = img[..., ::-1]
         img = img / 255. * 2 - 1
-        img_tensor = torch.from_numpy(img.transpose(2, 0, 1)).unsqueeze(0).to(self.device)
+        img_tensor = torch.from_numpy(
+            img.transpose(
+                2, 0, 1)).unsqueeze(0).to(
+            self.device)
         return img_tensor.float()
 
     def tenor2mask(self, tensor):
         if len(tensor.shape) < 4:
             tensor = tensor.unsqueeze(0)
         if tensor.shape[1] > 1:
-            tensor = tensor.argmax(dim=1) 
+            tensor = tensor.argmax(dim=1)
 
         tensor = tensor.squeeze(1).data.cpu().numpy()
         color_maps = []
